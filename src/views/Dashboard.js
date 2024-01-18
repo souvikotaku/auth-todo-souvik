a7d1b8c975740fb5679d839a7fb745bf050f1352
souvikotaku <manson1989@>
2024-01-18 16:49:57 +0530

    styling changes

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -42,7 +42,7 @@ const StyledContainer = styled(Container)({
   marginTop: "50px",
 });
 
-const StyledTodo = styled(ListItem)(({ theme }) => ({
+const StyledTodo = styled(ListItem)(({ theme, completed, isDeleting }) => ({
   display: "flex",
   justifyContent: "space-between",
   alignItems: "center",
@@ -50,16 +50,22 @@ const StyledTodo = styled(ListItem)(({ theme }) => ({
   border: "1px solid #ddd",
   borderRadius: "8px",
   marginBottom: "15px",
-  backgroundColor: "#f9f9f9",
+  backgroundColor: completed ? "#58D68D" : "#F2E79F", // Green color for completed, greyish yellow for incomplete
   width: "100%",
   maxWidth: "400px",
   maxWidth: "100% !important",
   transition: "background-color 0.3s, box-shadow 0.3s",
   cursor: "pointer",
-  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", // Added box shadow
+  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+
+  opacity: isDeleting ? 0 : 1, // Set opacity to 0 during deletion
+  maxHeight: isDeleting ? 0 : "100px", // Set max height to 0 during deletion
+  overflow: "hidden", // Hide overflow during deletion
+  transition: "opacity 0.5s, max-height 0.5s",
   "&:hover": {
-    backgroundColor: "#e8e8e8",
-    boxShadow: "0 8px 16px rgba(0, 0, 0, 0.2)", // Box shadow on hover
+    // Darker green color on hover for completed, lighter grey for incomplete
+    boxShadow: "0 8px 16px rgba(0, 0, 0, 0.2)",
+    marginTop: "-5px",
   },
   [theme.breakpoints.down("sm")]: {
     width: "80%",
@@ -67,16 +73,20 @@ const StyledTodo = styled(ListItem)(({ theme }) => ({
   },
 }));
 
-const TodoText = styled(ListItemText)({
+const TodoText = styled(ListItemText)(({ completed }) => ({
   flex: "1",
   wordBreak: "break-word",
   textOverflow: "ellipsis",
   "& > span": {
+    textDecoration: "none", // Remove text decoration from the entire text
     "&:hover": {
       textDecoration: "underline",
     },
+    "&:first-child": {
+      textDecoration: completed ? "line-through" : "none", // Apply line-through to the primary text
+    },
   },
-});
+}));
 const ButtonsContainer = styled("div")({
   display: "flex",
   alignItems: "center",
@@ -115,6 +125,7 @@ const Dashboard = () => {
     localStorage.removeItem("username");
     navigate("/");
   };
+  const [deletingIndex, setDeletingIndex] = useState(null);
 
   const [newTodo, setNewTodo] = useState("");
   const [editingIndex, setEditingIndex] = useState(null);
@@ -150,8 +161,14 @@ const Dashboard = () => {
   };
 
   const handleDeleteTodo = (index) => {
-    dispatch(deleteTodo(index));
-    notify("Todo deleted successfully");
+    setDeletingIndex(index);
+
+    // Delay the actual deletion to allow the animation to play
+    setTimeout(() => {
+      dispatch(deleteTodo(index));
+      notify("Todo deleted successfully");
+      setDeletingIndex(null); // Reset deleting index after deletion
+    }, 500);
   };
 
   const handleCompleteTodo = (index) => {
@@ -230,50 +247,55 @@ const Dashboard = () => {
         <Button onClick={handleAddTodo} variant="contained" color="primary">
           {editingIndex !== null ? "Edit Todo" : "Add Todo"}
         </Button>
-
-        <StyledGrid container spacing={2}>
-          {todoList.map((todo, index) => (
-            <Grid
-              item
-              md={3}
-              sm={12}
-              key={index}
-              className={isMobileView ? "mobile-todo-grid" : ""}
-            >
-              <StyledTodo key={index}>
-                <TodoText
-                  primary={todo?.text}
-                  secondary={todo?.completed ? "Completed" : "Incomplete"}
-                  style={{
-                    textDecoration: todo?.completed ? "line-through" : "none",
-                  }}
-                />
-                <ButtonsContainer>
-                  <IconButton
-                    onClick={() => handleEditTodo(index)}
-                    aria-label="edit"
-                  >
-                    <EditIcon />
-                  </IconButton>
-                  <IconButton
-                    onClick={() => handleDeleteTodo(index)}
-                    aria-label="delete"
-                  >
-                    <DeleteIcon />
-                  </IconButton>
-                  <IconButton
-                    onClick={() => handleCompleteTodo(index)}
-                    aria-label="complete"
-                  >
-                    <CheckCircleOutlineIcon
-                      style={{ color: todo?.completed ? "green" : "inherit" }}
-                    />
-                  </IconButton>
-                </ButtonsContainer>
-              </StyledTodo>
-            </Grid>
-          ))}
-        </StyledGrid>
+        <div className={"scrolldiv"}>
+          <StyledGrid container spacing={2}>
+            {todoList.map((todo, index) => (
+              <Grid
+                item
+                md={3}
+                sm={12}
+                key={index}
+                className={isMobileView ? "mobile-todo-grid" : ""}
+              >
+                <StyledTodo
+                  key={index}
+                  completed={todo?.completed}
+                  isDeleting={deletingIndex === index}
+                >
+                  <TodoText
+                    primary={todo?.text}
+                    secondary={todo?.completed ? "Completed" : "Incomplete"}
+                    completed={todo?.completed}
+                  />
+                  <ButtonsContainer>
+                    <IconButton
+                      onClick={() => handleEditTodo(index)}
+                      aria-label="edit"
+                      style={{ color: "#3498DB" }}
+                    >
+                      <EditIcon />
+                    </IconButton>
+                    <IconButton
+                      onClick={() => handleDeleteTodo(index)}
+                      aria-label="delete"
+                      style={{ color: "#EC7063" }}
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                    <IconButton
+                      onClick={() => handleCompleteTodo(index)}
+                      aria-label="complete"
+                    >
+                      <CheckCircleOutlineIcon
+                        style={{ color: todo?.completed ? "green" : "inherit" }}
+                      />
+                    </IconButton>
+                  </ButtonsContainer>
+                </StyledTodo>
+              </Grid>
+            ))}
+          </StyledGrid>
+        </div>
       </StyledContainer>
     </div>
   );
